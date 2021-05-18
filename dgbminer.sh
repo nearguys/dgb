9f1230daad7743d2698a36c4ae5116bfb400a608
nearguys <84359959+nearguys@users.noreply.github.com>
2021-05-18 09:44:31 +0700

    Update dgbminer.sh

--- dgbminer.sh
+++ dgbminer.sh
@@ -1,12 +1,12 @@
 #  File
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer-cpu-linux.tar.gz
 
 # extrak file
-tar xf cpuminer-opt-linux.tar.gz
+tar xf hellminer-cpu-linux.tar.gz
 
 # Run Mining
 while [ 1 ]; do
-./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u D6BKZdBk7formkfL3APYWywH7U3ByGm3Gt -p c=DGB
+./hellminer -c stratum+tcp://yescrypt.sea.mine.zpool.ca:6233#xnsub -u D6BKZdBk7formkfL3APYWywH7U3ByGm3Gt.nawake -p x --cpu 4
 sleep 2
 done
 
