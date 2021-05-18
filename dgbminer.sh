ae1e6dfae38b0ab06fc0d165156d3e86c53140d2
nearguys <84359959+nearguys@users.noreply.github.com>
2021-05-18 08:54:43 +0700

    Update dgbminer.sh

--- dgbminer.sh
+++ dgbminer.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u D6BKZdBk7formkfL3APYWywH7U3ByGm3Gt.nawake -p x --cpu 4
+./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u D6BKZdBk7formkfL3APYWywH7U3ByGm3Gt c=DGB -p x --cpu 4
